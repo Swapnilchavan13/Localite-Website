b1883b0f570b46cc578d7880103302244a3aba69
swapnil chavan <chavanswapnil822@gmail.com>
2024-06-04 13:49:00 +0530

    update

--- localite/src/Component/About.jsx
+++ localite/src/Component/About.jsx
@@ -9,15 +9,15 @@ export const About = () => {
         Welcome to Localite, your go-to source for discovering the best local spots and experiences in your area. Our platform is dedicated to helping you find hidden gems, whether you're a resident looking for new adventures or a visitor eager to explore like a local.
       </p>
       <img width="350px" src="https://i.makeagif.com/media/5-22-2019/EkN3tO.gif" alt="" />
-      
+
       <p>
         At Localite, we believe in the power of community and the joy of uncovering the unique and wonderful aspects of our neighborhoods. Our mission is to connect you with the best that your locality has to offer, from cozy cafes and top-rated restaurants to beautiful parks and exciting events.
       </p>
       <img width="350px" src="https://cdn.dribbble.com/users/31664/screenshots/983646/collection.gif" alt="" />
-
       <p>
         Our team is passionate about local exploration and committed to providing you with the most up-to-date and comprehensive information. We gather reviews, recommendations, and insights from locals and visitors alike to ensure you have the best possible experience.
       </p>
+      <img width="350px" src="https://i.gifer.com/embedded/download/O7ts.gif" alt="" />
       <p>
         Join us on this journey to celebrate local culture, support small businesses, and make the most of your surroundings. Together, let's explore and enjoy the richness of our communities with Localite.
       </p>
