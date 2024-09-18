79383e3d141401f0e073ed1319937bb5b2de1712
Swapnil Chavan <chavanswapnil822@gmail.com>
2024-09-18 17:46:01 +0530

    updted code only sticky navbar pending

--- localite/src/Component/Footer.jsx
+++ localite/src/Component/Footer.jsx
@@ -54,9 +54,13 @@ export const Footer = () => {
 <div className='lastline'>
     <p>@ 2024 Localite website | All rights reserved.</p>
     <div>
+        <a href="https://www.instagram.com/localite.india?igsh=bXA4M243eXhqMnI0" target='_blank'>
         <img src="insta.png" alt="" />
+        </a>
         <img src="fb.png" alt="" />
+        <a href="https://www.linkedin.com/company/103767681/admin/dashboard/" target='_blank'>
         <img src="in.png" alt="" />
+        </a>
         <img src="twitter.png" alt="" />
     </div>
 </div>
