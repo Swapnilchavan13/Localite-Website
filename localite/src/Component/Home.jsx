2d5d6f52756ed770be1e8271b7ac2c8cfcda505d
Swapnil Chavan <chavanswapnil822@gmail.com>
2024-10-17 12:55:57 +0530

    updated code

--- localite/src/Component/Home.jsx
+++ localite/src/Component/Home.jsx
@@ -233,23 +233,23 @@ export const Home = () => {
   <h1 className='intoline'>Our Network Partners</h1>
 
   <div className='top-row'>
-    <img src="6.png" data-aos="slide-right" alt="" />
-    <img src="4 (1).png" data-aos="zoom-in" alt="" />
-    <img src="11.png" data-aos="slide-right" alt="" />
-    <img src="10.png" data-aos="zoom-in" alt="" />
+      <img src="4 (1).png" alt="" />
+      <img src="9.png" alt="" />
+      <img src="7.png" alt="" />
+      <img src="5.png" alt="" />
   </div>
 
 
   <div className='scroll-container'>
   <img  className='scroll-btn' onClick={handleScrollImages} src="arr.png" alt="" />
     <div className='scroller'>
-      <img src="9.png" alt="" />
+      <img src="6.png" alt="" />
       <img src="8.png" alt="" />
+      <img src="11.png" alt="" />
       <img src="2.png" alt="" />
       <img src="1 (1).png" alt="" />
-      <img src="5.png" alt="" />
       <img src="3.png" alt="" />
-      <img src="7.png" alt="" />
+    <img src="10.png" alt="" />
     </div>
   </div>
 </div>
